880fb55654f1ba0c53e33e4858aa90d99315f1db
MAlexGG <alexandragalarzag@gmail.com>
2023-04-26 22:06:24 +0200

    fonts changed to font-face instead of importing from google fonts and video compressed

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { CtSlider, Video } from './Slider.styled';
-import VideoHomePage from '../../assets/videos/multimedia-1-home-factoriaf5-web_UJHvoRbY.mp4';
+import VideoHomePage from '../../assets/videos/VideoInicio.mp4';
 
 function Slider() {
   return (
